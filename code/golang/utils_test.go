d46e2512cdfdc023ef660a26bf3c58215dcdc6f7
leogtzr <leogutierrezramirez@gmail.com>
2020-02-05 10:41:46 -0700

    add test to validate question format

--- code/golang/utils_test.go
+++ code/golang/utils_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"regexp"
 	"testing"
 )
 
@@ -39,6 +40,28 @@ func Test_userInputToCmd(t *testing.T) {
 		got, _ := userInputToCmd(tc.input)
 		if got != tc.want {
 			t.Errorf("got=[%s], want=[%s]", got, tc.want)
+		}
+	}
+}
+
+func Test_questionHasValidFormat(t *testing.T) {
+	rgx := regexp.MustCompile("^\\d+@.+@(\\d+)?$")
+	type test struct {
+		input string
+		match bool
+	}
+
+	tests := []test{
+		{input: "1@Cómo puedes sortear un archivo?@2", match: true},
+		{input: "1@Cómo puedes sortear un archivo?@s", match: false},
+		{input: "@Cómo puedes sortear un archivo?@s", match: false},
+		{input: "1@x@2", match: true},
+		{input: "1@@2", match: false},
+	}
+	for _, tc := range tests {
+		match := rgx.MatchString(tc.input)
+		if match != tc.match {
+			t.Errorf("got=[%t], want=[%t]", match, tc.match)
 		}
 	}
 }
