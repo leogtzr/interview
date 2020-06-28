1a0ec953a8d018a37ab200863422bf9e8b57aeed
leogtzr <leogutierrezramirez@gmail.com>
2020-06-27 21:27:40 -0600

    remove unnecessary comments

--- code/golang/const.go
+++ code/golang/const.go
@@ -43,23 +43,22 @@ const (
 
 // Commands:
 const (
-	exitCmd             Command = iota
-	topicsCmd           Command = iota
-	helpCmd             Command = iota
-	useCmd              Command = iota
-	clearScreenCmd      Command = iota
-	pwdCmd              Command = iota
-	noCmd               Command = iota
-	startCmd            Command = iota
-	printCmd            Command = iota
-	nextQuestionCmd     Command = iota
-	previousQuestionCmd Command = iota
-	viewCmd             Command = iota
-	rightAnswerCmd      Command = iota
-	wrongAnswerCmd      Command = iota
-	mehAnswerCmd        Command = iota
-	finishCmd           Command = iota
-	//loadCmd                        Command = iota
+	exitCmd                        Command = iota
+	topicsCmd                      Command = iota
+	helpCmd                        Command = iota
+	useCmd                         Command = iota
+	clearScreenCmd                 Command = iota
+	pwdCmd                         Command = iota
+	noCmd                          Command = iota
+	startCmd                       Command = iota
+	printCmd                       Command = iota
+	nextQuestionCmd                Command = iota
+	previousQuestionCmd            Command = iota
+	viewCmd                        Command = iota
+	rightAnswerCmd                 Command = iota
+	wrongAnswerCmd                 Command = iota
+	mehAnswerCmd                   Command = iota
+	finishCmd                      Command = iota
 	increaseLevelCmd               Command = iota
 	decreaseLevelCmd               Command = iota
 	ignoreLevelCmd                 Command = iota
